5fc336fddc08ada1cde1d9d2acf90fedea2c3525
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-09 14:39:52 +0300

    Модул 03, Видео 03 - Работа със System.Math

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -13,10 +13,20 @@ namespace Modul03
 			Console.Write ("> b = ");
 			b = Convert.ToDouble (Console.ReadLine ());
 
+			//Форматиране с дименсия
 			string _format = "#0.00 м3";
 
+			//Степенуване
+			c = Math.Pow ( a, b );
+			Console.WriteLine ("\n\n>a ^ b = " + c.ToString ( _format ));
 
-			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString ( _format ));
+			//Kоренуване
+			c = Math.Sqrt ( b );
+			Console.WriteLine ("\n\n>b ^ 1/2 = " + c.ToString ( _format ));
+
+			//Закръгления
+			c = Math.Round ( a / b ) ;
+			Console.WriteLine ("\n\n>(a / b) = " + c.ToString () );
 		}
 	}
 }
