022eaa4058992e2182639937d8d50437a6285c5e
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-09 13:00:07 +0300

    Модул 03, Видео 01 - Променливи от тип Double

---
+++ Modul03/Modul03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double a = 0, b = 0, c = 0;
+
+			Console.Write ("> a = ");	a = Convert.ToDouble( Console.ReadLine () );
+			Console.Write ("> b = ");	b = Convert.ToDouble( Console.ReadLine () );
+
+
+
+			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString());
+		}
+	}
+}
