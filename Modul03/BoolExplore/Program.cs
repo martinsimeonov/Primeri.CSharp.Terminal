a17f43918143bf3f4874988348202f3a1d0e582a
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-09 15:07:49 +0300

    Модул 03, Видео 04 - Променливи от тип Bool

---
+++ Modul03/BoolExplore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BoolExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//bool _b = true;		// true / false
+
+			//bool _test = 5 > 10;
+
+			int[] _i = new int[] { 4, 3, 2};
+
+			int _ui = Convert.ToInt32 ( Console.ReadLine () );
+
+			bool _check01 = _ui <= _i.Length;
+			bool _check02 = _ui > 0;
+
+			Console.WriteLine ( _check01 );
+			Console.WriteLine ( _check02 );
+			Console.WriteLine ( _i [ _ui - 1] );
+		}
+	}
+}
