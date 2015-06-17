b3a05d9558eace2bb7320fa28686fdbaa18768c2
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-17 10:21:53 +0300

    Модул 03, Видео 06 - Работа с условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -14,20 +14,33 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32 ( Console.ReadLine () );
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02;		//Логическо и "&&"
+//			bool _exp02 = _check01 || _check02;		//Логическо или "||"
+//			bool _exp03 = !_check01;				//Отрицание "!"
+//
+//			Console.WriteLine ( "Логическо И: " + _exp01.ToString () );
+//			Console.WriteLine ( "Логическо ИЛИ: " + _exp02.ToString () );
+//			Console.WriteLine ( "Отрицание на " + _check01.ToString () + ": " + _exp03);
 
-			bool _exp01 = _check01 && _check02;		//Логическо и "&&"
-			bool _exp02 = _check01 || _check02;		//Логическо или "||"
-			bool _exp03 = !_check01;				//Отрицание "!"
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine ("Индекса е пракалено голям\n\n\n");
+				}
 
-			Console.WriteLine ( "Логическо И: " + _exp01.ToString () );
-			Console.WriteLine ( "Логическо ИЛИ: " + _exp02.ToString () );
-			Console.WriteLine ("Отрицание на " + _check01.ToString () + ": " + _exp03);
+				if (_ui <= 0 )
+				{
+					Console.WriteLine ("Индекса е пракалено малък\n\n\n");
+				}
+			}
 
 
 
-			//Console.WriteLine ( _i [ _ui - 1] );
 		}
 	}
 }
