571b6ac427774f7e7cf553318eb63a6be9ab0ae4
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-17 11:48:01 +0300

    Модул 03, Видео 10 - Управление на грешки

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -16,38 +16,53 @@ namespace BoolExplore2
 
 			bool _check01 = int.TryParse ( _input, out _ui );
 
+			try
+			{
+				Console.Write ("Елемент " + _ui.ToString () + " от масива е: ");
+				Console.WriteLine ( _i [ _ui - 1 ] );
+
+			}catch{
+				Console.WriteLine ( "<недефиниран елемент>" );
+			}
+
+
+
+//			Използване на условно присвояване
+
 //			_ui = ( _ui <= _i.Length ) ? _ui : 3;
 //			_ui = (_ui > 0) ? _ui : 1;
 
 
-			switch (_ui){
-			case 1:
-				{
-					Console.Write ("1ви елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			case 2:
-				{
-					Console.Write ("2ри елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			case 3:
-				{
-					Console.Write ("3ти елемент от масива: ");
-					Console.WriteLine (_i [_ui - 1]);
-					break;
-				}
-
-			default:
-				{
-					Console.Write ("В масива няма стойност за този индекс.\nОпитайте с индекс между 1 и 3.\n\n");
-					break;
-				}
-			}
+//			Използване на Switch
+//
+//			switch (_ui){
+//			case 1:
+//				{
+//					Console.Write ("1ви елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			case 2:
+//				{
+//					Console.Write ("2ри елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			case 3:
+//				{
+//					Console.Write ("3ти елемент от масива: ");
+//					Console.WriteLine (_i [_ui - 1]);
+//					break;
+//				}
+//
+//			default:
+//				{
+//					Console.Write ("В масива няма стойност за този индекс.\nОпитайте с индекс между 1 и 3.\n\n");
+//					break;
+//				}
+//			}
 
 
 
