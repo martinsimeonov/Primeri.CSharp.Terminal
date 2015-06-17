0c347e522a1d0882c3972b9c9af7208440b1b3ef
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-17 12:27:40 +0300

    Модул 04, Видео 02 - Примери с цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] _table = { "Иван", "Гошо", "Гергана"};
+
+			//Антетка на таблица
+			Console.WriteLine ("Име");
+			Console.WriteLine ("============");
+
+			//Тяло на таблица
+			for (int i = 0; i < _table.Length; i++ )
+			{
+				//Код на цикъла
+				Console.WriteLine ( _table [i] );
+			}  
+		}
+	}
+}
