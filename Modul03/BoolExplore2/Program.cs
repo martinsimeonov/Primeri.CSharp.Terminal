60140a3eb2e10ade91ccbdce3b0450f4bd22b9ff
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-17 11:09:29 +0300

    Модул 03, Видео 08 - Условно присвояване

---
+++ Modul03/BoolExplore2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoolExplore2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] _i = new int[] { 4, 3, 2};
+
+			int _ui = 0;
+			string _input = "0";
+
+			Console.Write ("Моля въведете индекс: ");
+			_input = Console.ReadLine ();
+
+			bool _check01 = int.TryParse ( _input, out _ui );
+
+			_ui = ( _ui <= _i.Length ) ? _ui : 3;
+			_ui = (_ui > 0) ? _ui : 1;
+
+
+			if (_check01) {
+				Console.WriteLine (_i [_ui - 1]);
+			}
+		}
+	}
+}
