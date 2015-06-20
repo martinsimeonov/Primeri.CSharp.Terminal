e1c88bbd60a7f765391ea6053e3396922bd32a48
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-20 14:28:19 +0300

    Модул 05, Видео 03 - Използване на масиви за io на методи

---
+++ Modul05/ClassExplorer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassExplorer
+{
+	public class person
+	{
+		public string simpleString = "тестова променлива от клас";
+
+		public person ()
+		{
+		}
+
+	}
+
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Modul05
 {
+
 	class MainClass
 	{
 		public static void Main (string[] args)
@@ -43,6 +44,14 @@ namespace Modul05
 						Console.WriteLine ( 0 );
 					}
 				}
+
+				if ( usercommand.Contains ( "cmd05" ) )
+				{
+					Console.WriteLine ( "Брой четни числа: " + cmd05 ( usercommand ).Length.ToString () );
+				}
+
+
+
 
 			}while ( usercommand != "exit" );
 
@@ -107,6 +116,35 @@ namespace Modul05
 			return (_temp > 0);
 		}
 
+		public static string[] cmd05 ( string _input )
+		{
+			try
+			{
+				int _border = 0;
+				string _temp = "";
+
+				if ( int.TryParse ( _input.Split (' ')[1], out _border))
+				{
+					for ( int i = 0; i < _border; i++)
+					{
+						if ( i % 2 == 0 )	_temp += i.ToString ();
+
+						if ( i != _border - 1 &&  i % 2 == 0 )	_temp += ",";
+
+					}
+
+					Console.WriteLine ( _temp );		//само за проверка
+
+					return _temp.Split (',');
+				}
+
+
+			}catch{
+			}
+
+			return new string[1] { "0" };
+		}
+			
 
 
 	}
