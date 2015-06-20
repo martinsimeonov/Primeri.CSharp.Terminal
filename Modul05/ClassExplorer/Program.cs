ebe5f9c819b07f997934d7bdd51195b6c2bc785c
Мартин Симеонов <simeonov.martin@gmail.com>
2015-06-20 14:49:33 +0300

    Модул 05, Видео 05 - Дефиниране на провменливи в клас и нива на достъп

--- Modul05/ClassExplorer/Program.cs
+++ Modul05/ClassExplorer/Program.cs
@@ -5,10 +5,23 @@ namespace ClassExplorer
 	public class person
 	{
 		public string simpleString = "тестова променлива от клас";
+		public string user = "";
 
-		public person ()
+		private string hiddenString = "";
+
+		public person (string _ini)
 		{
-			simpleString = "Промяна на променлива при инициализиране.";
+			if (_ini == "password1") hiddenString = "Потребител 1"; 
+			if (_ini == "password2") hiddenString = "Потребител 2";
+
+			user = hiddenString;
+
+			if (hiddenString.Length > 0)
+				simpleString = "Добре дошли отново, " + user + "!";
+			else
+				simpleString = "Вие нямате достъп до тази програма";
+
+
 		}
 
 
@@ -21,11 +34,16 @@ namespace ClassExplorer
 		
 		public static void Main (string[] args)
 		{
+			Console.Write ("Парола: "); string _user = Console.ReadLine ();
+
 			//Дефиниране на клас
-			person _person = new person();
+			person _person = new person(_user);
 
 			//Достъпване на клас
 			Console.WriteLine ( _person.simpleString );
+			_person.simpleString;
+			_person.user;
+
 		}
 	}
 }
